52127d58e6e64611bb0872c568fbd1e10d0598d2
Maple <maple@mmquant.net>
2017-08-10 14:05:03 +0200

    upgrade to d3v4

--- finished/src/components/data-circles.jsx
+++ finished/src/components/data-circles.jsx
@@ -1,17 +1,20 @@
+// unfinished/src/components/data-circles.jsx
 import React from 'react';
 
-const renderCircles = (props) => {
+const renderCircles = props => {
   return (coords, index) => {
     const circleProps = {
       cx: props.xScale(coords[0]),
       cy: props.yScale(coords[1]),
-      r: 2,
+      r: 3,
       key: index
     };
     return <circle {...circleProps} />;
   };
 };
 
-export default (props) => {
-  return <g>{ props.data.map(renderCircles(props)) }</g>
-}
+const DataCircles = props => {
+  return <g>{props.data.map(renderCircles(props))}</g>;
+};
+
+export default DataCircles;
--- finished/src/index.jsx
+++ finished/src/index.jsx
@@ -1,14 +1,16 @@
 import './main.css';
-import React         from 'react';
-import ReactDOM      from 'react-dom';
-import { Provider }  from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { makeStore } from './lib/store';
-import Chart         from './components/chart.jsx';
+import Chart from './components/chart.jsx';
 
 const store = makeStore();
-const app = <Provider store={store}>
-  <Chart />
-</Provider>
+const app = (
+  <Provider store={store}>
+    <Chart />
+  </Provider>
+);
 
 const mountingPoint = document.createElement('div');
 mountingPoint.className = 'react-app';
